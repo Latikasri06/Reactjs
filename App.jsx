c0bc8d9015391454ee186141f678dc5394411e73
Latika sri S <latikasri2007@gmail.com>
2026-01-12 10:27:48 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,66 @@
+import {useState} from 'react'
+
+function App() {
+  const[name,setName]=useState("");
+  const[email,setEmail]=useState("");
+  const[password,setPassword]=useState("");
+  const[submittedData,setSUbmittedData]=useState(null);
+  const handleSubmit=(e)=>{
+    e.preventDefault();
+    const data={name,email,password};
+    setSUbmittedData(data);
+    setName("");
+    setEmail("");
+    setPassword("");
+  };
+
+  return (
+    <div className='card'>
+    <h1>Registration Form</h1>
+    <form onSubmit={handleSubmit}>
+      <label>Name</label>
+      <input
+      type="text"
+      className="input-box"
+      value={name}
+      onChange={(e)=>setName(e.target.value)}
+      required
+      />
+      <label>Email</label>
+      <input
+      type="email"
+      className="input-box"
+      value={email}
+      onChange={(e)=>setEmail(e.target.value)}
+      required
+      />
+      <label>Password</label>
+      <input
+      type="password"
+      className="input-box"
+      value={password}
+      onChange={(e)=>setPassword(e.target.value)}
+      required
+      />
+      <button type="submit" className="btn">
+        submit
+      </button>
+
+    </form>
+    {submittedData && (
+      <div className="output-card">
+        <h2>submittedData</h2>
+        <p><strong>Name:</strong>{submittedData.name}</p>
+        <p><strong>Email:</strong>{submittedData.email}</p>
+        <p><strong>Password:</strong>{submittedData.password}</p>
+    
+      
+    </div>
+  )
+}
+</div>
+);
+}
+
+
+export default App;
